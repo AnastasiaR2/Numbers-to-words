9847594a029b6d6352e35aeefe1dd4a7d5d25ebd
AnastasiaR2 <mascheracapelli@gmail.com>
2022-06-30 16:40:51 +0300

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,17 +9,17 @@
 
   // let num = +prompt('Введите число:');
 
-  let num = 643;
+  let num = 11;
 
   function hrivna(num){
-    let lastDigitStr = String(num).slice(-1);
-    let lastDigitNum = Number(lastDigitStr);
-    if (lastDigitNum == 1) {
-      return 'гривна';
-    }else if (lastDigitNum == 2 || lastDigitNum == 3){
-      return 'гривны';
-    }else {
+    let lastDigit = +String(num).slice(-1);
+    let twoLastDigit = +String(num).slice(-2);
+    if (lastDigit > 3 && lastDigit <= 9 || lastDigit == 0 || twoLastDigit == 11) {
       return 'гривен';
+    }else if (lastDigit == 2 || lastDigit == 3){
+      return 'гривны';
+    }else if (lastDigit == 1){
+      return 'гривна';
     }
   }
 
@@ -42,21 +42,37 @@
   }
   
   function convertThousands(num) {
-    if (num >= 1000) {
-      return convertHundreds(Math.floor(num / 1000)) + ' тысяч ' + convertHundreds(num % 1000); 
-    }else {
+    if (num >= 1000 && num < 2000) {
+      return convertHundreds(Math.floor(num / 1000)) + ' тысяча ' + convertHundreds(num % 1000); 
+    }else if (num >= 2000 && num < 5000){
+      return convertHundreds(Math.floor(num / 1000)) + ' тысячи ' + convertHundreds(num % 1000);
+    }else if (num >= 5000){
+      return convertHundreds(Math.floor(num / 1000)) + ' тысяч ' + convertHundreds(num % 1000);
+    }else{
       return convertHundreds(num);
     }
   }
 
   function convertMillions(num) {
-    if (num >= 1000000 && num <= 999999999){
-      units.splice(1, 2, 'один', 'два');
-      return convertThousands(Math.floor(num / 1000000)) + ' миллионов ' + convertThousands(num % 1000000);
+    if (num >= 1000000 && num < 2000000){
+      return convertThousands(Math.floor(num / 1000000)) + ' миллион ' + convertThousands(num % 1000000);
+    }else if (num >= 2000000 && num < 5000000){
+      return convertThousands(Math.floor(num / 1000000)) + ' миллиона ' + convertThousands(num % 1000000);
+    }else if (num >= 5000000 && num <= 999999999){
+      return convertThousands(Math.floor(num / 1000000)) + ' миллионнов ' + convertThousands(num % 1000000);
     }else {
       return convertThousands(num);
     }
   }
+
+  function main() {
+  var cases = [1, 2, 10, 11, 19, 20, 21, 22, 27, 50, 99, 100, 101, 110, 111, 1000, 2011, 4000, 5000, 1000011, 2000000, 3000000, 5000000, 999999999];
+  for (var i = 0; i < cases.length; i++) {
+    console.log(cases[i] + ": " + convertMillions(cases[i]) + ' ' + hrivna(cases[i]));
+  }
+}
+
+  main();
 
   console.log(convertMillions(num) + ' ' + hrivna(num));
 
